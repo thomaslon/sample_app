f21179455abdb83c62e446ccdd36a21d527a9b22
Paullondon <paullondon@arcor.de>
2014-01-24 15:32:27 +0100

    Finish user signup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ SampleApp::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  # Speed up tests by lowering bcrypt's cost function.
+  ActiveModel::SecurePassword.min_cost = true
 end
